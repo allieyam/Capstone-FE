52b6563c14dee9f551104ec01230d89d21650047
Allie Yam <alethea.yam@gmail.com>
2022-10-25 15:36:23 +0800

    committing to pull main

---
+++ src/Components/Sentiment/SentimentPop.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = { results: any };
+
+export default function SentimentPop({ results }: Props) {
+  return (
+    <div>
+      SentimentPop
+      {/* map data out as list*/}
+    </div>
+  );
+}
--- src/Components/Templates/template1.tsx
+++ src/Components/Templates/template1.tsx
@@ -8,6 +8,7 @@ import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 import "98.css";
 import Sentiment from "../Sentiment/Sentiment";
+import SentimentPop from "../Sentiment/SentimentPop";
 
 //Define the interface here
 interface Style {
@@ -44,7 +45,7 @@ function Template1({
   const [userBlurb, setUserBlurb] = useState("");
   console.log("user work", userWork, work);
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
-
+  const [brainData, setBrainData] = useState<any[]>();
   const toggleClicked = () => {
     setToggle(false);
   };
@@ -109,7 +110,8 @@ function Template1({
 
   function dataML() {
     console.log("user work in data ml", work);
-    Sentiment([work]);
+    const data = Sentiment([work]);
+    setBrainData(data);
   }
 
   // const generatePDF = () => {
@@ -119,247 +121,261 @@ function Template1({
   //   });
   // };
   return (
-    <div className="template" id="divToPrint">
-      <div className="template1-container">
-        <div className="template1-header-container">
-          <div className="template1-name">
-            {toggle ? (
-              username
-            ) : (
-              <input
-                className=" text-gray-700 font-bold"
-                type="text"
-                name="name"
-                placeholder="name"
-                defaultValue={username}
-                onChange={(event) => updateAll("name", event.target.value)}
-              ></input>
-            )}
-          </div>
-          <div className="template1-contact-container">
-            <div className="template1-email">
-              {/* <FontAwesomeIcon icon="far fa-envelope" /> */}
-              📧
+    <div className="big-template-container">
+      <div className="template" id="divToPrint">
+        <div className="template1-container">
+          <div className="template1-header-container">
+            <div className="template1-name">
               {toggle ? (
-                email
+                username
               ) : (
-                <input
-                  className=" text-gray-700 font-bold"
-                  type="text"
-                  name="email"
-                  placeholder="email"
-                  value={email}
-                  onChange={(event) => updateAll("email", event.target.value)}
-                ></input>
-              )}
-            </div>
-            <div className="template1-phone">
-              📱
-              {toggle ? (
-                phone
-              ) : (
-                <input
-                  className=" text-gray-700 font-bold"
-                  type="text"
-                  name="contact"
-                  placeholder="contact"
-                  value={phone}
-                  onChange={(event) => updateAll("contact", event.target.value)}
-                ></input>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="template1-blurb">
-          {toggle ? (
-            userSummary
-          ) : (
-            <input
-              className=" text-gray-700 font-bold"
-              type="text"
-              name="summary"
-              placeholder="summary"
-              value={userSummary}
-              onChange={(event) => updateAll("summary", event.target.value)}
-            ></input>
-          )}
-        </div>
-        <div className="template1-bottom-headers">Work Experience</div>
-        <div className="template1-work">
-          {work &&
-            work.map((company, index) => {
-              return toggle ? (
-                <div className="template1-work-section" key={index}>
-                  <div className="template1-work-header">
-                    <div className="template1-place">
-                      {company.place}&nbsp; <i> {company.position}</i>
-                    </div>
-                    <div className="template1-dates">
-                      {company.date_started}-{company.date_ended}
-                    </div>
-                  </div>
-                  {company.description}
-                  <br />
-                </div>
-              ) : (
-                <div className="template1-educationplace" key={index}>
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="text"
-                    name="place"
-                    placeholder="COMPANY"
-                    value={company.place}
-                    onChange={(event) =>
-                      updateWork(index, "place", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="text"
-                    name="description"
-                    placeholder="DESCRIPTION"
-                    value={company.description}
-                    onChange={(event) =>
-                      updateWork(index, "description", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="date"
-                    name="date_started"
-                    placeholder="date_started"
-                    value={company.date_started}
-                    onChange={(event) =>
-                      updateWork(index, "date_started", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="date"
-                    name="date_ended"
-                    placeholder="date_ended"
-                    value={company.date_ended}
-                    onChange={(event) =>
-                      updateWork(index, "date_ended", event.target.value)
-                    }
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="template1-bottom-headers">Education</div>
-        <div className="template1-education">
-          {education &&
-            education.map((educationPlace, index) => {
-              return toggle ? (
-                <div className="template1-education-section" key={index}>
-                  <div className="template1-education-header">
-                    <div className="template1-place">
-                      {educationPlace.place}&nbsp;{" "}
-                      <i> {educationPlace.level}</i>
-                    </div>
-                    <div className="template1-dates">
-                      {educationPlace.date_started}-{educationPlace.date_ended}
-                    </div>
-                  </div>
-
-                  {educationPlace.description}
-                  <br />
-                </div>
-              ) : (
-                <div className="template1-educationplace" key={index}>
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="text"
-                    name="place"
-                    placeholder="place"
-                    value={educationPlace.place}
-                    onChange={(event) =>
-                      updateEducation(index, "place", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="text"
-                    name="description"
-                    placeholder="DESCRIPTION"
-                    value={educationPlace.description || " "}
-                    onChange={(event) =>
-                      updateEducation(index, "description", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="date"
-                    name="date_started"
-                    placeholder="date_started"
-                    value={educationPlace.date_started || ""}
-                    onChange={(event) =>
-                      updateEducation(index, "date_started", event.target.value)
-                    }
-                  />
-                  <br />
-                  <input
-                    className=" text-gray-700 font-bold"
-                    type="date"
-                    name="date_ended"
-                    placeholder="date_ended"
-                    value={educationPlace.date_ended || ""}
-                    onChange={(event) =>
-                      updateEducation(index, "date_ended", event.target.value)
-                    }
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="template1-bottom-headers">Key Skills</div>
-        {keyskills &&
-          keyskills.map((keySk, index) => {
-            return toggle ? (
-              <div className="template1-skill" key={index}>
-                <div className="template1-place"> {keySk.name}</div>
-                {keySk.description}
-              </div>
-            ) : (
-              <div>
                 <input
                   className=" text-gray-700 font-bold"
                   type="text"
                   name="name"
-                  placeholder="NAME"
-                  value={keySk.name || ""}
-                  onChange={(event) =>
-                    updateSkill(index, "name", event.target.value)
-                  }
-                />
-                <br />
-                <input
-                  className=" text-gray-700 font-bold"
-                  type="text"
-                  name="description"
-                  value={keySk.description || ""}
-                  placeholder="DESCRIPTION"
-                  onChange={(event) =>
-                    updateSkill(index, "description", event.target.value)
-                  }
-                />
+                  placeholder="name"
+                  defaultValue={username}
+                  onChange={(event) => updateAll("name", event.target.value)}
+                ></input>
+              )}
+            </div>
+            <div className="template1-contact-container">
+              <div className="template1-email">
+                {/* <FontAwesomeIcon icon="far fa-envelope" /> */}
+                📧
+                {toggle ? (
+                  email
+                ) : (
+                  <input
+                    className=" text-gray-700 font-bold"
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(event) => updateAll("email", event.target.value)}
+                  ></input>
+                )}
               </div>
-            );
-          })}
+              <div className="template1-phone">
+                📱
+                {toggle ? (
+                  phone
+                ) : (
+                  <input
+                    className=" text-gray-700 font-bold"
+                    type="text"
+                    name="contact"
+                    placeholder="contact"
+                    value={phone}
+                    onChange={(event) =>
+                      updateAll("contact", event.target.value)
+                    }
+                  ></input>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="template1-blurb">
+            {toggle ? (
+              userSummary
+            ) : (
+              <input
+                className=" text-gray-700 font-bold"
+                type="text"
+                name="summary"
+                placeholder="summary"
+                value={userSummary}
+                onChange={(event) => updateAll("summary", event.target.value)}
+              ></input>
+            )}
+          </div>
+          <div className="template1-bottom-headers">Work Experience</div>
+          <div className="template1-work">
+            {work &&
+              work.map((company, index) => {
+                return toggle ? (
+                  <div className="template1-work-section" key={index}>
+                    <div className="template1-work-header">
+                      <div className="template1-place">
+                        {company.place}&nbsp; <i> {company.position}</i>
+                      </div>
+                      <div className="template1-dates">
+                        {company.date_started}-{company.date_ended}
+                      </div>
+                    </div>
+                    {company.description}
+                    <br />
+                  </div>
+                ) : (
+                  <div className="template1-educationplace" key={index}>
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="text"
+                      name="place"
+                      placeholder="COMPANY"
+                      value={company.place}
+                      onChange={(event) =>
+                        updateWork(index, "place", event.target.value)
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="text"
+                      name="description"
+                      placeholder="DESCRIPTION"
+                      value={company.description}
+                      onChange={(event) =>
+                        updateWork(index, "description", event.target.value)
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="date"
+                      name="date_started"
+                      placeholder="date_started"
+                      value={company.date_started}
+                      onChange={(event) =>
+                        updateWork(index, "date_started", event.target.value)
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="date"
+                      name="date_ended"
+                      placeholder="date_ended"
+                      value={company.date_ended}
+                      onChange={(event) =>
+                        updateWork(index, "date_ended", event.target.value)
+                      }
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div className="template1-bottom-headers">Education</div>
+          <div className="template1-education">
+            {education &&
+              education.map((educationPlace, index) => {
+                return toggle ? (
+                  <div className="template1-education-section" key={index}>
+                    <div className="template1-education-header">
+                      <div className="template1-place">
+                        {educationPlace.place}&nbsp;{" "}
+                        <i> {educationPlace.level}</i>
+                      </div>
+                      <div className="template1-dates">
+                        {educationPlace.date_started}-
+                        {educationPlace.date_ended}
+                      </div>
+                    </div>
+
+                    {educationPlace.description}
+                    <br />
+                  </div>
+                ) : (
+                  <div className="template1-educationplace" key={index}>
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="text"
+                      name="place"
+                      placeholder="place"
+                      value={educationPlace.place}
+                      onChange={(event) =>
+                        updateEducation(index, "place", event.target.value)
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="text"
+                      name="description"
+                      placeholder="DESCRIPTION"
+                      value={educationPlace.description || " "}
+                      onChange={(event) =>
+                        updateEducation(
+                          index,
+                          "description",
+                          event.target.value
+                        )
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="date"
+                      name="date_started"
+                      placeholder="date_started"
+                      value={educationPlace.date_started || ""}
+                      onChange={(event) =>
+                        updateEducation(
+                          index,
+                          "date_started",
+                          event.target.value
+                        )
+                      }
+                    />
+                    <br />
+                    <input
+                      className=" text-gray-700 font-bold"
+                      type="date"
+                      name="date_ended"
+                      placeholder="date_ended"
+                      value={educationPlace.date_ended || ""}
+                      onChange={(event) =>
+                        updateEducation(index, "date_ended", event.target.value)
+                      }
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div className="template1-bottom-headers">Key Skills</div>
+          {keyskills &&
+            keyskills.map((keySk, index) => {
+              return toggle ? (
+                <div className="template1-skill" key={index}>
+                  <div className="template1-place"> {keySk.name}</div>
+                  {keySk.description}
+                </div>
+              ) : (
+                <div>
+                  <input
+                    className=" text-gray-700 font-bold"
+                    type="text"
+                    name="name"
+                    placeholder="NAME"
+                    value={keySk.name || ""}
+                    onChange={(event) =>
+                      updateSkill(index, "name", event.target.value)
+                    }
+                  />
+                  <br />
+                  <input
+                    className=" text-gray-700 font-bold"
+                    type="text"
+                    name="description"
+                    value={keySk.description || ""}
+                    placeholder="DESCRIPTION"
+                    onChange={(event) =>
+                      updateSkill(index, "description", event.target.value)
+                    }
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <button onClick={() => printDocument()}>Print</button>
+        <br />
+        {toggle ? (
+          <button onClick={() => toggleClicked()}>Edit</button>
+        ) : (
+          <button onClick={(e) => handleSubmit(e)}>Submit</button>
+        )}
       </div>
-      <button onClick={() => printDocument()}>Print</button>
-      <br />
-      {toggle ? (
-        <button onClick={() => toggleClicked()}>Edit</button>
-      ) : (
-        <button onClick={(e) => handleSubmit(e)}>Submit</button>
-      )}
+      <SentimentPop results={brainData} />
     </div>
   );
 }
